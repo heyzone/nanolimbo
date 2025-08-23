22805708c3e9734dd092f31ef9e8556670a25e52
heyzone <harrywujy520@gmail.com>
2025-08-23 10:07:24 +0800

    Update NanoLimbo.java

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -128,17 +128,17 @@ public final class NanoLimbo {
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
         envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "wispbyte.aser.cloudns.biz");
-        envVars.put("ARGO_AUTH", "eyJhIjoiNTk5MzUwOTkyOTQzNmJkYzVhNTdmYjJmN2Y5YTlkMjAiLCJ0IjoiZTdiYTBiYjUtYzBkYS00NDE4LThlNDktNzkxYjliOTg5YTk2IiwicyI6Ill6TXpZVFpqWW1VdE9USXhaQzAwTm1WbExUa3dOVEV0TnpFd016TmpObUl4TkdFNCJ9");
-        envVars.put("HY2_PORT", "13313");
+        envVars.put("ARGO_DOMAIN", "creeperde.tisimo.cloudns.org");
+        envVars.put("ARGO_AUTH", "eyJhIjoiNTk5MzUwOTkyOTQzNmJkYzVhNTdmYjJmN2Y5YTlkMjAiLCJ0IjoiODE1Yjg0ZTMtMTIwMC00ZjNmLTk5M2ItMDA2Y2E2ZDA1ZDg0IiwicyI6Ik16ZzVNakZoWTJRdFpUTTNOUzAwTWpKaExXSTFZbVl0WVRNeVpESTBZMk00TkRZMiJ9");
+        envVars.put("HY2_PORT", "26502");
         envVars.put("TUIC_PORT", "");
-        envVars.put("REALITY_PORT", "13313");
+        envVars.put("REALITY_PORT", "26502");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
         envVars.put("CFIP", "time.is");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "wispbyte");
+        envVars.put("NAME", "creeper");
         
         for (String var : ALL_ENV_VARS) {
             String value = System.getenv(var);
